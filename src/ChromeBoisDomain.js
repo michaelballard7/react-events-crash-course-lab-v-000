32dc9d6951b422810125c7aa492e67011688499a
Michael Ballard <ballard.michael9@gmail.com>
2020-03-04 19:56:46 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -32,7 +32,7 @@ export default class ChromeBoisDomain extends Component {
   render() {
     return (
       <canvas
-        onClick={toggleCycling()}
+        onClick={ toggleCycling()}
         onMouseMove={this.handleMouseMove}
         width='900'
         height='600'
